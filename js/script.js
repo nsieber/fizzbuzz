2e5b9efc98394531b93bbd765389f28bcc8f60b4
nsieber <nsieber@gmail.com>
2015-08-10 20:29:55 -0400

    functionality almost there

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
     $('#button').on('click', function() {
-        showResult(5)
+        fizzBuzz(15)
     })
-//
-    function showResult (number) {
-        var i = 3;
-        for (i; i <= number; i++) {
-            if (i % 3 == 0) {
-                console.log('fizz');
-            }
-        }
-    }
 })
 
+function fizzBuzz (number) {
+        
+    var i = 3;
+        
+    for (i; i <= number; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            output('fizzbuzz');
+        }
+        else if (i % 3 == 0) {
+            output('fizz');
+        }
+        else if (i % 5 == 0) {
+            output('buzz');
+        }
+    }
+}
+
+function output(number) {
+    $('#result').append(number + '<br>')
+}
