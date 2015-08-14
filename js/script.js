5208a1be97571a6e3b8db5459458c9de46522172
nsieber <nsieber@gmail.com>
2015-08-13 20:59:29 -0400

    validate user inputs a number

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
 $(document).ready(function() {
     $('#button').on('click', function() {
-        fizzBuzz($('#number').val())
+        fizzBuzz ($('#number').val())
     })
 })
 
 function fizzBuzz (number) {
-        
-    var i = 1;
-        
-    for (i; i <= number; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            output('fizzbuzz');
+    if (parseInt(number)) {
+        number = parseInt(number)
+//        number = +number
+        var i = 1;
+            
+        for (i; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                output('fizzbuzz');
+            }
+            else if (i % 3 == 0) {
+                output('fizz');
+            }
+            else if (i % 5 == 0) {
+                output('buzz');
+            }
+            else {
+                output(i);
+            }
         }
-        else if (i % 3 == 0) {
-            output('fizz');
-        }
-        else if (i % 5 == 0) {
-            output('buzz');
-        }
-        else {
-            output(i);
-        }
+    }
+    else {
+        alert('Please enter a number')
     }
 }
 
